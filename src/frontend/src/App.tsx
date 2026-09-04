378056a62d758ae9c70e6ab54439f6e4137b801c
PaperKreker <98393012+PaperKreker@users.noreply.github.com>
2026-09-04 21:44:29 +0300

    Added checkbox style

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { MainPage } from './pages/MainPage';
+import {Components} from "./pages/Components";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
       </header>
       <main className="App-main">
-        <MainPage></MainPage>
+        <Components></Components>
       </main>
     </div>
   );
---
+++ src/frontend/src/pages/Components.tsx
@@ -0,0 +1,16 @@
+export function Components() {
+    return (
+        <div className="App">
+            <h1>H1 Заголовок</h1>
+            <h2>H2 Заголовок</h2>
+            <h3>H3 Заголовок</h3>
+            <h4>H4 Заголовок</h4>
+            <p>Тестовый параграф</p>
+            <button>Тестовая кнопа</button>
+            <button className={"success"}>Успех</button>
+            <button className={"error"}>Ошибка</button>
+            <input type="checkbox"/>
+            <input type="radio"/>
+        </div>
+    )
+}
