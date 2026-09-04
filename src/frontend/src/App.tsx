a631c4728270617d96fe9fec4e3783d8d7e0a79e
PaperKreker <98393012+PaperKreker@users.noreply.github.com>
2026-09-04 20:25:51 +0300

    Added buttons style

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { MainPage } from './pages/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main className="App-main">
+        <MainPage></MainPage>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/frontend/src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+export function MainPage() {
+    return (
+        <div className="App">
+            <p>Test</p>
+            <button>Тестовая кнопа</button>
+            <button className={"success"}>Успех</button>
+            <button className={"error"}>Ошибка</button>
+        </div>
+    )
+}
